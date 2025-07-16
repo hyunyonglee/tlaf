cc22d5cd683c8f72c13116c34846aff68a369af1
hyunyonglee <hyunyong.rhee@gmail.com>
2025-07-16 21:59:38 +0900

    Gamma term added

--- model.py
+++ model.py
@@ -19,6 +19,7 @@ class TLAF(CouplingModel,MPOModel):
         Ly = model_params.get('Ly', 2)
         Jxx = model_params.get('Jxx', 1.)
         Jz = model_params.get('Jz', 1.)
+        G = model_params.get('G', 0.)
         hz = model_params.get('hz', 0.)
         bc_MPS = model_params.get('bc_MPS', 'finite')
         bc = model_params.get('bc', 'periodic')
@@ -32,8 +33,27 @@ class TLAF(CouplingModel,MPOModel):
 
         # NN XXZ
         for u1, u2, dx in self.lat.pairs['nearest_neighbors']:
+            # print(f"Adding coupling between {u1} and {u2} with dx={dx}")
+        
             self.add_coupling(Jxx, u1, 'Sx', u2, 'Sx', dx)
             self.add_coupling(Jxx, u1, 'Sy', u2, 'Sy', dx)
             self.add_coupling(Jz, u1, 'Sz', u2, 'Sz', dx)
         
+            # Gamma terms
+            if np.all(dx == np.array([1, 0])):
+                self.add_coupling( G, u1, 'Sy', u2, 'Sz', dx)
+                self.add_coupling( G, u1, 'Sz', u2, 'Sy', dx)
+
+            elif np.all(dx == np.array([-1, 1])):
+                self.add_coupling( G * np.cos(2*np.pi/3.), u1, 'Sy', u2, 'Sz', dx)
+                self.add_coupling( G * np.cos(2*np.pi/3.), u1, 'Sz', u2, 'Sy', dx)
+                self.add_coupling( -G * np.sin(2*np.pi/3.), u1, 'Sx', u2, 'Sz', dx)
+                self.add_coupling( -G * np.sin(2*np.pi/3.), u1, 'Sz', u2, 'Sx', dx)
+
+            else:
+                self.add_coupling( G * np.cos(2*np.pi/3.), u1, 'Sy', u2, 'Sz', dx)
+                self.add_coupling( G * np.cos(2*np.pi/3.), u1, 'Sz', u2, 'Sy', dx)
+                self.add_coupling( G * np.sin(2*np.pi/3.), u1, 'Sx', u2, 'Sz', dx)
+                self.add_coupling( G * np.sin(2*np.pi/3.), u1, 'Sz', u2, 'Sx', dx)
+            
         MPOModel.__init__(self, lat, self.calc_H_MPO())
