41027a0a786c4be61e7f676b76db4b109a1bbcb3
hyunyonglee <hyunyong.rhee@gmail.com>
2025-12-04 14:07:08 +0900

    test

--- run_dmrg_skx.py
+++ run_dmrg_skx.py
@@ -225,9 +225,9 @@ if __name__ == "__main__":
                 idx = (y - phase_shift) % 3
                 if idx == 0:  # U U
                     # product_state.append(np.array([1+0.7, 1]))
-                    product_state.append(np.array([1, 0]))
+                    product_state.append(np.array([1+0.1, +0.1]))
                 elif idx == 1:  # U U
-                    product_state.append(np.array([1, 0]))
+                    product_state.append(np.array([1+0.1, -0.1]))
                 else:              # D
                     product_state.append(np.array([0, 1]))
 
